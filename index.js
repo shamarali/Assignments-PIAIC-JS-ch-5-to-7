45ec72058342bdce48387f2fcda253f9fa2f7284
Shamar Ali <111586356+shamarali@users.noreply.github.com>
2023-03-06 15:14:34 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+let a = 2;
+let b = a++ + ++a - --a + a--;
+
+alert("Value of a: " + a); // Displays "Value of a: 2" in an alert box
+alert("Value of b: " + b); // Displays "Value of b: 6" in an alert box
